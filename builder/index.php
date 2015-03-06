8759b8e8cab3c153aa34c3b7b168e212e4cec17b
Renan Vaz <renan.c.vaz@gmail.com>
2015-03-06 02:10:47 -0300

    Working...

--- builder/index.php
+++ builder/index.php
@@ -5,6 +5,8 @@ ini_set('phar.readonly', 'Off');
 ini_set('phar.require_hash', 'Off');
 error_reporting(E_ALL | E_STRICT);
 
+@unlink('U.phar');
+
 $phar = new Phar('U.phar');
 $phar->buildFromDirectory('../src/');
 $phar->setDefaultStub('cli.php');
--- src/cli.php
+++ src/cli.php
@@ -1,11 +1,11 @@
 <?php
 
-Phar::mapPhar();
-
 include 'phar://U.phar/U.php';
 
-print_r($argv);
+echo UCore::test();
 
 //parse_str(implode('&', array_slice($argv, 1)), $_GET);
+
+echo "\n";
 
 __HALT_COMPILER();
