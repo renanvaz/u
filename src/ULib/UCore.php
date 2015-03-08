4d21fa60f81fd9bd76c9ccd507786dacb77ceb3a
Renan Vaz <renan.c.vaz@gmail.com>
2015-03-08 04:57:40 -0300

    Working...

--- src/ULib/UCore.php
+++ src/ULib/UCore.php
@@ -6,33 +6,6 @@ require_once('U.php');
 
 class UCore {
     /**
-     * Init the U pointer
-     * @return void
-     */
-
-    public static function proccess ($reports) {
-        $status = TRUE;
-
-        foreach ($reports['reports'] as &$report) {
-            if (isset($report['reports'])) {
-                $report = self::proccess($report);
-
-                if (!$report['status']) {
-                    $status = FALSE;
-                }
-            } else {
-                if (!$report['status']) {
-                    $status = FALSE;
-                }
-            }
-        }
-
-        $reports['status'] = $status;
-
-        return $reports;
-    }
-
-    /**
      * Load a file with tests and create a group for this
      * @param  string $file Filename relative or absolute
      * @return void
@@ -57,27 +30,5 @@ class UCore {
      */
     public static function getJSON () {
         return json_encode(U::getReport());
-    }
-
-    /**
-     * Load a view
-     * @param  string $filename
-     * @param  array $data
-     * @return string
-     */
-    public static function view ($filename, $data) {
-        extract($data, EXTR_SKIP);
-
-        ob_start();
-
-        try {
-            include $filename;
-        } catch (Exception $e) {
-            ob_end_clean();
-
-            throw $e;
-        }
-
-        return ob_get_clean();
     }
 }
